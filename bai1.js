4e8635d4882a1672f78b7844ef5721f9b63cb911
Dang Quy <dangquy95php@gmail.com>
2020-05-23 10:30:10 +0700

    commit

--- bai1.js
+++ bai1.js
@@ -24,3 +24,16 @@ data1.map(str=> {
         console.log(str);
     }
 })
+
+let data2 = [
+    'this is a dog',
+    'this is a doy123',
+    'this is a dog213'
+];
+
+// /b là đại diện cho ranh giới giữa 1 ký tự \w và 1 ký tự ko phải chữ 
+data2.map(str=> {
+    if (str.match(/\bdog\b/g)) {
+        console.log(str);
+    }
+})
